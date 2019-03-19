b491373e3c5b57303f211c1652c24ec2a93d827b
satak <sami.koskivaara@gmail.com>
2019-03-19 15:43:15 +0200

    fix deploy.ps1

--- deploy.ps1
+++ deploy.ps1
@@ -1,7 +1,7 @@
 param (
     [Parameter(Mandatory)]
     [string]$Project,
-	$Region="europe-west1"
+    [string]$Region="europe-west1"
 )
 
 gcloud functions deploy notification --runtime python37 --trigger-topic notification --project $Project --region $Region --env-vars-file env.yaml --source ./src
